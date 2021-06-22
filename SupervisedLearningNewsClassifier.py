ed00f7a3ea528bbcfee70dc90a93fd206b9f7412
dylan4d <72990303+dylan4d@users.noreply.github.com>
2021-06-22 12:11:49 +0100

    Create SupervisedLearningNewsClassifier.py

---
+++ SupervisedLearningNewsClassifier.py
@@ -0,0 +1,22 @@
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer
+
+emails = fetch_20newsgroups()
+
+#print(emails.target_names)
+
+train_emails = fetch_20newsgroups(categories = ['comp.sys.ibm.pc.hardware', 'comp.sys.mac.hardware'], subset='train', shuffle='True', random_state = 108)
+
+test_emails = fetch_20newsgroups(categories = ['comp.sys.ibm.pc.hardware', 'comp.sys.mac.hardware'], subset='test', shuffle='True', random_state = 108)
+
+counter = CountVectorizer()
+counter.fit(test_emails.data + train_emails.data)
+
+train_counts = counter.transform(train_emails.data)
+test_counts = counter.transform(test_emails.data)
+
+classifier = MultinomialNB()
+classifier.fit(train_counts, train_emails.target)
+print(classifier.score(test_counts, test_emails.target))
+
